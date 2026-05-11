79fbb5ead039f268a92a39b117aa0bc3dc298d41
sergio ivan fernandez galeano <sercolgi1@gmail.com>
2026-05-10 19:24:23 -0500

    ejercicio 7 multiplo de 3

---
+++ taller02funciones/punto7multiplode3.js
@@ -0,0 +1,9 @@
+function multiploDe3(numero) {
+    let esMultiplo3 = false;
+    if (numero % 3 === 0) {
+        esMultiplo3 = true
+    }
+    return esMultiplo3
+}
+
+console.log(multiploDe3(5))
